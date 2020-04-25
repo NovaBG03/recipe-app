bf4423b2da28ea998ca1aefc6ac933169c1fc956
Nikita Koynov <nikicha03@gmail.com>
2020-04-25 22:41:56 +0300

    added UnitOfMeasure entity

--- src/main/java/com/example/recipeapp/domain/Ingredient.java
+++ src/main/java/com/example/recipeapp/domain/Ingredient.java
@@ -17,6 +17,9 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure unitOfMeasure;
+
     public Long getId() {
         return id;
     }
