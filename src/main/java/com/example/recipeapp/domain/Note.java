132602e84c409632b8138e69b68ff336ba5175ac
Nikita Koynov <nikicha03@gmail.com>
2020-04-25 22:11:28 +0300

    note and recipe entities added

---
+++ src/main/java/com/example/recipeapp/domain/Note.java
@@ -0,0 +1,17 @@
+package com.example.recipeapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String notes;
+
+    @OneToOne
+    private Recipe recipe;
+}
