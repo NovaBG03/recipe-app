d2c8ee23b44c37d59c631c43b6f20526466e6160
Nikita Koynov <nikicha03@gmail.com>
2020-04-26 02:00:18 +0300

    added data jpa query methods

--- src/main/java/com/example/recipeapp/controllers/IndexController.java
+++ src/main/java/com/example/recipeapp/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package com.example.recipeapp.controllers;
 
+import com.example.recipeapp.domain.Category;
+import com.example.recipeapp.domain.UnitOfMeasure;
+import com.example.recipeapp.repositories.CategoryRepository;
+import com.example.recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        Optional<Category> optionalCategory = categoryRepository.findByName("American");
+        Optional<UnitOfMeasure> optionalUnitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat id: " + optionalCategory.get().getId());
+        System.out.println("UOF id: " + optionalUnitOfMeasure.get().getId());
+
         return "index";
     }
 }
