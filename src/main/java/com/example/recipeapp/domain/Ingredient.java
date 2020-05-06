3a55af653cc505aff48c9a583b7db206ebad8275
Nikita Koynov <nikicha03@gmail.com>
2020-05-06 12:01:48 +0300

    added bootstrap to index page

--- src/main/java/com/example/recipeapp/domain/Ingredient.java
+++ src/main/java/com/example/recipeapp/domain/Ingredient.java
@@ -2,8 +2,6 @@ package com.example.recipeapp.domain;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
