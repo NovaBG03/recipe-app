685fe37b41c41b1f6a6fa215445e89a41a500f4d
Nikita Koynov <nikicha03@gmail.com>
2020-05-06 14:09:16 +0300

    added argument captor to IndexController test

--- src/test/java/com/example/recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/example/recipeapp/controllers/IndexControllerTest.java
@@ -4,6 +4,7 @@ import com.example.recipeapp.domain.Recipe;
 import com.example.recipeapp.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
@@ -33,17 +34,27 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
-        Recipe mockRecipe = new Recipe();
+        //given
         Set<Recipe> mockRecipes = new HashSet<>();
-        mockRecipes.add(mockRecipe);
+        for (long i = 1L; i <= 2; i++) {
+            Recipe mockRecipe = new Recipe();
+            mockRecipe.setId(i);
+            mockRecipes.add(mockRecipe);
+        }
 
         when(recipeService.getAllRecipes()).thenReturn(mockRecipes);
 
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
         String viewName = indexController.getIndexPage(model);
 
+        //then
         assertEquals("index", viewName);
-
         verify(recipeService, times(1)).getAllRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        Set<Recipe> capturedRecipes = argumentCaptor.getValue();
+        assertEquals(mockRecipes.size(), capturedRecipes.size());
     }
 }
