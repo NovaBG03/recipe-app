64aed53d99005fdc49b3b0e9fe3483d5433dd483
Nikita Koynov <nikicha03@gmail.com>
2020-05-11 23:08:26 +0300

    updated IT to Junit5

--- src/test/java/com/example/recipeapp/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/com/example/recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -3,17 +3,16 @@ package com.example.recipeapp.repositories;
 import com.example.recipeapp.domain.UnitOfMeasure;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
+@ExtendWith({SpringExtension.class})
 @DataJpaTest
 class UnitOfMeasureRepositoryIT {
 
